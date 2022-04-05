2518966dd9ddd474c443e06b21e852b349a045fc
Praveenchan5 <praveenjnrs97@gmail.com>
2022-04-05 08:57:44 +0530

    new

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,33 @@
+package org.phone;
+
+public class PhoneInfo {
+
+
+			public void phoneName() {
+		System.out.println("realmex3 superzoom");
+		}
+		public void phoneMieiNum() {
+			System.out.println("8538746217347");
+		}
+		public void camera() {
+			System.out.println("64mp");
+		}
+public void  storage() {
+	System.out.println("128gb");
+}
+public void osName() {
+	System.out.println("android 11");
+}
+		public static void main(String[] args)  {
+			//ClassName objectName = new ClassName();
+	        PhoneInfo	p = new PhoneInfo();
+	//objectname.methodName();
+	p.phoneName();
+	p.phoneMieiNum();
+	p.camera();
+	p.storage();
+	p.osName();
+	}	
+		}
+
+
